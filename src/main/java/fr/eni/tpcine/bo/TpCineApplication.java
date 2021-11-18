0f2c0f68f8e5e688a8d51975cec343af4659c56e
Jean-Baptiste BLASCHKA <jeanbaptiste.blaschka2020@campus-eni.fr>
2021-11-18 17:10:25 +0100

    test2

---
+++ src/main/java/fr/eni/tpcine/bo/TpCineApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.tpcine.bo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpCineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpCineApplication.class, args);
+	}
+
+}
