4e273cb4780697a44e10d56ac395922ebbf516ed
Dmoulart <dorian.moulart@gmail.com>
2021-11-19 09:42:29 +0100

    Add home page and templates

---
+++ src/main/java/fr/eni/tpcine/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package fr.eni.tpcine.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "pages/index";
+	}
+}
